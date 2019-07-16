6acd37ca160ed6cd1c3bce506d76e9620276d6f9
Pchenrique <pchenrique1998@hotmail.com>
2019-07-16 00:46:45 -0300

    Criação da relação de usuario com o home 1 para 1

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function home(){
+        return $this->hasOne('App\Home');
+    }
 }
