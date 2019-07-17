44f4bb152a03faaaa4a77d9c4f396b9d14d0ffaf
TIEverton <evertonpinheiroti@hotmail.com>
2019-07-16 23:57:50 -0300

    Criação da tela de apresentação e folha de estilo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html>     
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -18,9 +18,11 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
+        <h1 class="text-hide" style="background-image: url('http://pngimg.com/uploads/bitcoin/bitcoin_PNG48.png');">Bootstrap</h1>
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,92 +8,55 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style_welcome.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+<div class="parallax">
+<nav class="navbar navbar-expand-lg static-top">
+  <div class="container">
+                <!-- LOGO -->
+        <a class="navbar-brand" href="#">
+            <img  src="img/logo.png" width="215" height="76" class="d-inline-block align-top" alt="">
+        </a>
 
+                <!-- BOTÃO MOVEL -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+          @if (Route::has('login'))
+                    @auth
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bolder" href="{{ url('/home') }}">Home</a>
+                        <span class="sr-only">(current)</span>
+                    </li>
+                    @else
+
+                    <li class="nav-item">
+                        <a style="" class="nav-link font-weight-light text-white" href="{{ route('login') }}"><span class="detalhes_barra_titulo">ENTRAR</span></a>
+                        <span class="sr-only">(current)</span>
+                    </li>
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <li class="nav-item"> 
+                                <a class="nav-link font-weight-light text-white" href="{{ route('register') }}"><span class="detalhes_barra_titulo">REGISTRAR-SE</span></a>
+                                <span class="sr-only">(current)</span>
+                            </li>
                         @endif
                     @endauth
                 </div>
             @endif
+      </ul>
+  </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+</nav>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+         
+         
     </body>
 </html>
