3d3bbec1ec1be31f5ba87755c79a3237ee2278a8
TIEverton <evertonpinheiroti@hotmail.com>
2019-07-19 01:11:52 -0300

    Front-end das telas de confirmar email/redefinir senha

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,51 @@
-@extends('layouts.app')
+        @extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>YourCoins - Redefinir Senha</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/style_welcome.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style_login.css') }}" rel="stylesheet">
+
+        <!-- script -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        <div class="parallax">
+        <nav class="navbar navbar-expand-lg static-top">
+          <div class="container">
+                        <!-- LOGO -->
+                <a class="navbar-brand" href="/yourcoins/public/">
+                    <img  src="../../img/logo.png" width="215" height="76" class="d-inline-block align-top" alt="">
+                </a>
+
+                        <!-- BOTÃO MOVEL -->
+                <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="glyphicon glyphicon-search"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              
+          </div>
+
+        </nav>
+ 
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="row justify-content-center my-5">
+        <div class="col-md-5">
+            <div class="card text-white" style="background-color: rgba(0,0,0,.8);">
+                <div class="card-header" style="border-color: #ed8200;">{{ __('Redefinir senha') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -13,48 +53,37 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
+                  
+                            <div class="col-md-12 form-label-group">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" placeholder="Endereço de Email" autofocus>
+                                <label for="email">Endereço de Email</label>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                        
+                            <div class="col-md-12 form-label-group">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Digite uma nova senha">
+                                <label for="password">Digite uma nova senha</label>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="col-md-12 form-label-group">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Repita a senha digitada">
+                                <label for="password-confirm">Repita a senha digitada</label>
                             </div>
-                        </div>
+                        <div class="col-md-12">
+                           <button class="btn btn-outline-info btn-block text-white" id="btn-login-reset" type="submit">{{ __('Redefinir senha') }}</button>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
                         </div>
                     </form>
                 </div>
@@ -63,3 +92,7 @@
     </div>
 </div>
 @endsection
+
+    </body>
+</html>
+
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,11 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>YourCoins</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/style_welcome.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- script -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        <div class="parallax">
+        <nav class="navbar navbar-expand-lg static-top">
+          <div class="container">
+                        <!-- LOGO -->
+                <a class="navbar-brand" href="/yourcoins/public/">
+                    <img  src="../img/logo.png" width="215" height="76" class="d-inline-block align-top" alt="">
+                </a>
+
+                        <!-- BOTÃO MOVEL -->
+                <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="glyphicon glyphicon-search"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              
+          </div>
+
+        </nav>
+         
+    </body>
+</html>
+
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+            <div class="card text-white" style="background-color: rgba(0,0,0,.8);">
+                <div class="card-header" style="border-color: #ed8200;">{{ __('Verifique seu endereço de email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
